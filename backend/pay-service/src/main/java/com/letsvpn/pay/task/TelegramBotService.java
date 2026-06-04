251383d7e25162eba4d975261e6007eca2a642f4
solo <chibatobeijing@gmail.com>
2026-06-04 18:07:57 +0800

    telegrambot

--- backend/pay-service/src/main/java/com/letsvpn/pay/task/TelegramBotService.java
+++ backend/pay-service/src/main/java/com/letsvpn/pay/task/TelegramBotService.java
@@ -27,11 +27,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Service
+@ConditionalOnProperty(name = "telegram-bot.enabled", havingValue = "true")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class TelegramBotService implements UpdatesListener {
 
